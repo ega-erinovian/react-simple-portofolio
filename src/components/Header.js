40278790f27eab88a482ce2f3104742ed19755ad
egaputra69 <egaputra71@gmail.com>
2021-08-27 19:30:48 +0700

    header completed

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <div className="container-fluid">
+        <h1>I Am Tokio</h1>
+        <h2>Graphic Designer & Photographer</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
